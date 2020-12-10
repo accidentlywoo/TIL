db19dc8564ebdad4b1ed8d2bff0505c55595d959
maeve <maeve.woo@goodoc.co.kr>
2020-12-11 02:07:47 +0900

    구현해라 오반아

--- JavaStudy-WhiteShip/Week4-Controll/README.md
+++ JavaStudy-WhiteShip/Week4-Controll/README.md
@@ -9,7 +9,7 @@
 - 이미 JUnit 알고 계신분들은 다른 것 아무거나!
 - 더 자바, 테스트 강의도 있으니 참고하세요~
 
-1. 과제 live-study 대시 보드를 만드는 코드를 작성하세요.
+1. [과제 live-study 대시 보드를 만드는 코드를 작성하세요.](#1-과제-live-study-대시-보드를-만드는-코드를-작성하세요)
 - 깃헙 이슈 1번부터 18번까지 댓글을 순회하며 댓글을 남긴 사용자를 체크 할 것.
 - 참여율을 계산하세요. 총 18회에 중에 몇 %를 참여했는지 소숫점 두자리가지 보여줄 것.
 - Github 자바 라이브러리를 사용하면 편리합니다.
@@ -22,12 +22,12 @@
 - ListNode remove(ListNode head, int positionToRemove)를 구현하세요.
 - boolean contains(ListNode head, ListNode nodeTocheck)를 구현하세요.
 
-3. Stack을 구현하세요.
+3. [Stack을 구현하세요.](#3-stack을-구현하세요)
 - int 배열을 사용해서 정수를 저장하는 Stack을 구현하세요.
 - void push(int data)를 구현하세요.
 - int pop()을 구현하세요.
 
-4. 앞서 만든 ListNode를 사용해서 Stack을 구현하세요.
+4. [앞서 만든 ListNode를 사용해서 Stack을 구현하세요.](#4-앞서-만든-listnode를-사용해서-stack을-구현하세요)
 - ListNode head를 가지고 있는 ListNodeStack 클래스를 구현하세요.
 - void push(int data)를 구현하세요.
 - int pop()을 구현하세요.
@@ -43,8 +43,20 @@
 - 깃헙 API를 익명으로 호출하는데 제한이 있기 때문에 본인의 깃헙 프로젝트에 이슈를 만들고 테스트를 하시면 더 자주 테스트할 수 있습니다.
 
 ## 2. LinkedList를 구현하세요.
-- LinkedList에 대해 공부하세요.
+### LinkedList에 대해 공부하세요.
+
+    상속 구조 : 
+    1. Object
+        : clone, equals, getClass, hashCpde, toString, wait,notify
+    2. AbstractCollection 
+        : add, addAll, clear, contains, isEmpty, iterator, remove, removeAll, toArray
+    3. AbstractList
+    4. AbstractSequentialList
+        : 
+    5. LinkedList 
+
     메모리의 물리적 위치가 주어지지 않는 데이터 요소들의 선형 집합이다.
+
 - 정수를 저장하는 ListNode 클래스를 구현하세요.
 - ListNode add(ListNode head, ListNode nodeToAdd, int position)를 구현하세요.
 - ListNode remove(ListNode head, int positionToRemove)를 구현하세요.
@@ -61,3 +73,8 @@
 - int pop()을 구현하세요.
   
 ## 5. Queue를 구현하세요.
+
+---
+
+### 참고 사이트
+- [Java 11 API](https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/LinkedList.html)
---
+++ JavaStudy-WhiteShip/Week4-Controll/src/main/java/ListNode.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+	1. Linked List에대해 공부하세요잉
+	2. 정수를 저장하는 ListNode 클래스를 구현하라
+	3. ListNode add(ListNode head, ListNode nodeToAdd, int position)
+	4. ListNode remove(ListNode head, int positionToRemove)
+	5. boolean contains(ListNode head, ListNode nodeTocheck)
+ */
+
+/**
+ * Doble Linked List
+ */
+public class ListNode {
+	private int[] node;
+	private final List<int[]> list;
+
+	private int listLen;
+
+	public ListNode(){
+		node = new int[3]; // node[0] : front Link , node[1] : data , node[2] : end Link
+		list = new ArrayList<int[]>();
+		listLen = list.size();
+	}
+
+	private int[] getNode(){
+		return new int[3];
+	}
+
+	/**
+	 * add end of list
+	 * @param data
+	 * @return
+	 */
+	public ListNode add(int data){
+		int[] node = getNode();
+		if(listLen-1 > 0) {
+			node[0] = list.get(listLen-1)[2];
+			node[2] = list.get(listLen-1)[2] ++;
+		}
+		else {
+			node[0] = 1;
+			node[2] = 0;
+		}
+		node[1] = data;
+		list.add(node);
+		listLen++;
+		return this;
+	}
+
+	/**
+	 *
+	 * @param head -> position
+	 * @param data
+	 * @param position == 0 ? front : end
+	 * @return
+	 */
+	public ListNode add(int head, int data, int position){
+		for(int i = 0; i < listLen; i++) {
+			int[] ints = getNode();
+			if(list.get(i)[0] == head){
+				ints[0] = list.get(i)[0];
+				ints[2] = list.get(i)[2];
+				for (int j = head+position+1; j < listLen; j++ ){
+					list.get(j)[0] += 1;
+					list.get(j)[2] += 1;
+				}
+			}else{
+				ints[0] = list.get(listLen-1)[0];
+				ints[2] = list.get(listLen-1)[2];
+			}
+
+			ints[1] = data;
+			listLen++;
+			list.add(ints);
+		}
+
+		return this;
+	}
+
+	public ListNode remove(int head, int data, int position){
+
+		return this;
+	}
+
+	public boolean contains(ListNode head, ListNode nodeTocheck){
+		return false;
+	}
+}
