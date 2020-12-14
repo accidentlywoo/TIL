a15d380ddb3cbdac69735545778a118f3b432227
maeve <maeve.woo@goodoc.co.kr>
2020-12-14 23:13:48 +0900

    프로잭트 다시세틴

---
+++ JavaStudy-WhiteShip/java-study/src/main/java/com/doit/javastudy/JavaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.doit.javastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyApplication.class, args);
+	}
+
+}
---
+++ JavaStudy-WhiteShip/java-study/src/test/java/com/doit/javastudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doit.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
