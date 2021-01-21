d6a8590b35954e4cb60664380eaaf7ef0ef355c9
accidentlywoo <accidentlywoo@gmail.com>
2021-01-21 22:24:44 +0900

    리눅스 달인이 되고야 말겠어

---
+++ JavaStudy-WhiteShip/java-study/src/main/java/com/doit/javastudy/algorithm/B1215Level2/조합경우의수.java
@@ -0,0 +1,22 @@
+package com.doit.javastudy.algorithm.B1215Level2;
+
+public class 조합경우의수 {
+    public static String solution(int[] numbers){
+        String answer = "";
+        int len = numbers.length;
+        int max = 0;
+        String[] chapchap = new String[len];
+        for(int i = 0; i < len; i++){
+            if(max > numbers[i]){
+                
+            }
+        }
+
+//        chapchap[i] = String.valueOf(numbers[i]);
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        solution(new int[]{1,2,3});
+    }
+}
