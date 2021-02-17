b913d29ea75e1ee673f07dc1cf91139825186887
accidentlywoo <accidentlywoo@gmail.com>
2021-02-17 16:11:31 +0900

    비트연산자 신난다

---
+++ JavaStudy-WhiteShip/java-study/src/main/java/com/doit/javastudy/classes/BitOperation.java
@@ -0,0 +1,11 @@
+package com.doit.javastudy.classes;
+
+public class BitOperation {
+	public static void main(String[] args) {
+		int num1 = 000001010;
+		int num2 = 000000101;
+		System.out.println(num1 & num2);
+		System.out.println(num1 | num2);
+		System.out.println(num1 ^ num2); // 1111 7+
+	}
+}
