9861235136f05066ff267c67f8bad71283b82f2c
accidentlywoo <accidentlywoo@gmail.com>
2021-02-17 16:13:04 +0900

    비트 표시

--- JavaStudy-WhiteShip/java-study/src/main/java/com/doit/javastudy/classes/BitOperation.java
+++ JavaStudy-WhiteShip/java-study/src/main/java/com/doit/javastudy/classes/BitOperation.java
@@ -2,8 +2,8 @@ package com.doit.javastudy.classes;
 
 public class BitOperation {
 	public static void main(String[] args) {
-		int num1 = 000001010;
-		int num2 = 000000101;
+		int num1 = 0B0000_01010;
+		int num2 = 0B0000_00101;
 		System.out.println(num1 & num2);
 		System.out.println(num1 | num2);
 		System.out.println(num1 ^ num2); // 1111 7+
